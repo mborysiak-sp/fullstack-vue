190021794ae0e3e00894744d7c631e6e621a6f0f
mborysiak-sp <marcinez24@gmail.com>
2020-05-26 21:57:45 +0200

    Added login form

--- Project/src/components/Navbar.vue
+++ Project/src/components/Navbar.vue
@@ -13,3 +13,25 @@
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+.navbar {
+  .nav {
+    background-color: #ddd;
+    padding: 1em;
+    padding-left: $left-padding;
+  }
+  .cont {
+    margin: 1em;
+  }
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    font-family: sans-serif;
+    color: #777;
+    &.router-link-exact-active {
+      color: cornflowerblue;
+    }
+  }
+}
+</style>
---
+++ Project/src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <div class="row">
+      <h4>Login</h4>
+      <form>
+        <label for="username">Username</label>
+        <input type="text"
+          id="username"
+          placeholder="Username"
+          name="username"
+          v-model="username"
+          class="form-control">
+        <label for="password">Password</label>
+        <input type="password"
+          id="password"
+          placeholder="Password"
+          name="password"
+          v-model="password"
+          class="form-control">
+          <button class="login-button">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  username: "",
+  password: ""
+};
+</script>
+
+<style>
+
+</style>
