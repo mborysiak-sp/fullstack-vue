4d91a11bb8a68eb9a3b5bcbbb18c576f15fb33b6
mborysiak-sp <marcinez24@gmail.com>
2020-05-27 19:20:32 +0200

    Update Navbar.vue
    Added link to login

--- Project/src/components/Navbar.vue
+++ Project/src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <div class="navbar">
     <div class="nav">
       <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
     </div>
     <div class="cont">
       <router-view />
